3c168909e3f11d98b6011b4b03fef1c7c289273d
Tom Brennan <tjb1982@gmail.com>
2017-07-31 19:15:18 -0400

    0.1.0

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject co.nclk/clj-yaml "0.1.0-SNAPSHOT"
+(defproject co.nclk/clj-yaml "0.1.0"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "https://github.com/tjb1982/clj-yaml"
   :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo
-            :comments "same as Clojure"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
